e1ff9d25f293258fabaf47ba9086c63ea65cce78
Edgar <edgar.007.armando@gmail.com>
2022-01-30 23:07:22 -0600

    Proyecto final procesamiento

--- dist/src/index.js
+++ dist/src/index.js
@@ -6,15 +6,15 @@ canvas.height = window.innerHeight;
 var numberOfParticles = 500;
 var ParticlesArrays = [];
 var plantilla = new Image();
-var file = document.getElementById('fileupload');
-file.addEventListener('change', function () {
-    plantilla.src = URL.createObjectURL(file.files[0]);
+var lienzo1 = document.getElementById('fileupload');
+lienzo1.addEventListener('change', function () {
+    plantilla.src = URL.createObjectURL(lienzo1.files[0]);
 });
-var Particle = /** @class */ (function () {
-    function Particle() {
-        this.radius = Math.random() * 200 + 20;
+var ParticleRotation = /** @class */ (function () {
+    function ParticleRotation() {
+        this.size = Math.random() * 200 + 20;
         this.x = Math.random() * canvas.width;
-        this.y = canvas.height + this.radius * 2;
+        this.y = canvas.height + this.size * 2;
         this.speedY = Math.random() * 5 + 1;
         this.speedX = Math.random() * 3 - 1.5;
         this.angle = Math.random() * 360;
@@ -24,39 +24,37 @@ var Particle = /** @class */ (function () {
         this.frameY = Math.floor(Math.random() * 3);
         this.spriteSize = 900 / 3;
     }
-    Particle.prototype.draw = function () {
+    ParticleRotation.prototype.draw = function () {
         ctx.save();
         ctx.translate(this.x, this.y);
         ctx.rotate(this.angle * Math.PI / 360 * this.spin);
         ctx.drawImage(plantilla, this.frameX * this.spriteSize, this.frameY
             * this.spriteSize, this.spriteSize, this.spriteSize, 0 - this.
-            radius / 2, 0 - this.radius / 2, this.radius, this.radius);
-        ctx.translate(-this.x, -this.x);
+            size / 2, 0 - this.size / 2, this.size, this.size);
         ctx.restore();
     };
-    Particle.prototype.update = function () {
+    ParticleRotation.prototype.update = function () {
         this.angle += 5;
         this.y -= this.speedY;
         this.x += this.speedX;
-        if (this.radius > 1)
-            this.radius -= 0.5;
+        if (this.size > 1)
+            this.size -= 0.5;
     };
-    return Particle;
+    return ParticleRotation;
 }());
 function init() {
     for (var i = 0; i < numberOfParticles; i++) {
-        ParticlesArrays.push(new Particle());
+        ParticlesArrays.push(new ParticleRotation());
     }
 }
 init();
 function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     if (ParticlesArrays.length < numberOfParticles) {
-        ParticlesArrays.push(new Particle);
+        ParticlesArrays.push(new ParticleRotation);
     }
-    connect();
     for (var i = 0; i < ParticlesArrays.length; i++) {
-        if (ParticlesArrays[i].radius <= 1) {
+        if (ParticlesArrays[i].size <= 1) {
             ParticlesArrays.splice(i, 1);
         }
         ParticlesArrays[i].update();
@@ -65,13 +63,3 @@ function animate() {
     requestAnimationFrame(animate);
 }
 animate();
-function connect() {
-    for (var i = 0; i < ParticlesArrays.length; i++) {
-        ctx.strokeStyle = 'red';
-        ctx.lineWidth = ParticlesArrays[i].radius / 5;
-        ctx.beginPath();
-        ctx.moveTo(ParticlesArrays[i].x, ParticlesArrays[i].y);
-        ctx.stroke();
-        ctx.closePath();
-    }
-}
--- src/index.ts
+++ src/index.ts
@@ -3,33 +3,33 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const numberOfParticles = 500;
-let ParticlesArrays:Particle []=[];
+let ParticlesArrays:ParticleRotation []=[];
 let plantilla = new Image();
 
 
-const file =<HTMLInputElement>document.getElementById('fileupload');
+const lienzo1 =<HTMLInputElement>document.getElementById('fileupload');
 
-file.addEventListener('change',function(){
-    plantilla.src = URL.createObjectURL(file.files[0]);
+lienzo1.addEventListener('change',function(){
+    plantilla.src = URL.createObjectURL(lienzo1.files[0]);
 });
 
 
-class Particle {
-    radius: number;
-    x: number;
-    y: number;
-    speedY: number;
-    speedX: number;
-    angle: number;
-    spin: number;
-    frameX: number;
-    frameY: number;
-    spriteSize: number;
+class ParticleRotation {
+    public size: number;
+    public x: number;
+    public y: number;
+    public speedY: number;
+    public speedX: number;
+    public angle: number;
+    public spin: number;
+    public frameX: number;
+    public frameY: number;
+    public spriteSize: number;
 
     constructor(){
-        this.radius = Math.random() * 200 + 20;
+        this.size = Math.random() * 200 + 20;
         this.x = Math.random() * canvas.width;
-        this.y = canvas.height + this.radius * 2;
+        this.y = canvas.height + this.size * 2;
         this.speedY = Math.random() * 5 + 1;
         this.speedX = Math.random() * 3 - 1.5;
         this.angle = Math.random() * 360;
@@ -39,39 +39,35 @@ class Particle {
         this.frameY =Math.floor(Math.random() * 3);
         this.spriteSize = 900/3;
     }
-
     draw(){
         ctx.save();
         ctx.translate(this.x, this.y);
         ctx.rotate(this.angle * Math.PI/360 * this.spin);
         ctx.drawImage(plantilla,this.frameX * this.spriteSize, this.frameY
-            * this.spriteSize, this.spriteSize, this.spriteSize, 0 - this.
-            radius/2, 0 - this.radius/2, this.radius, this.radius);
-        ctx.translate(-this.x,-this.x);
+        * this.spriteSize, this.spriteSize, this.spriteSize, 0 - this.
+        size/2, 0 - this.size/2, this.size, this.size);
         ctx.restore();
     }
     update(){
         this.angle +=5;
         this.y -= this.speedY;
         this.x += this.speedX;
-        if(this.radius>1)this.radius -= 0.5;
+        if(this.size>1)this.size -= 0.5;
     }  
 }
 function init(){
     for(let i = 0; i < numberOfParticles; i++){
-        ParticlesArrays.push(new Particle());
+        ParticlesArrays.push(new ParticleRotation());
     }
 }
 init();
-
 function animate(){
     ctx.clearRect(0,0, canvas.width,canvas.height);
     if(ParticlesArrays.length < numberOfParticles){
-        ParticlesArrays.push(new Particle);
+        ParticlesArrays.push(new ParticleRotation);
     }
-    connect();
     for(let i = 0; i < ParticlesArrays.length; i++){
-        if(ParticlesArrays[i].radius <= 1){
+        if(ParticlesArrays[i].size <= 1){
             ParticlesArrays.splice(i, 1)
         }
         ParticlesArrays[i].update();
@@ -80,15 +76,3 @@ function animate(){
     requestAnimationFrame(animate);
 }
 animate();
-
-function connect(){
-    for(let i=0; i < ParticlesArrays.length; i++){
-        ctx.strokeStyle = 'red';
-        ctx.lineWidth = ParticlesArrays[i].radius/5;
-        ctx.beginPath();
-        ctx.moveTo(ParticlesArrays[i].x, ParticlesArrays[i].y);
-        
-        ctx.stroke();
-        ctx.closePath();
-    }
-}
